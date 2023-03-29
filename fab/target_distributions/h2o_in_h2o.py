5246c9f6b9cdd0579e483b4e0083e7aec4586f44
Timsey <tim_bb@hotmail.com>
2023-03-29 18:53:54 +0200

    water system setting up

--- fab/target_distributions/h2o_in_h2o.py
+++ fab/target_distributions/h2o_in_h2o.py
@@ -28,9 +28,8 @@ class WaterInWaterBox(TestSystem):
         use mass repartitioning.
 
     """
-    def __init__(self, constraints=app.HBonds, hydrogenMass=None, **kwargs):
+    def __init__(self, **kwargs):
         TestSystem.__init__(self, **kwargs)
-
         # http://docs.openmm.org/latest/userguide/application/02_running_sims.html
         # Two methods: either create system from pdb and FF with forcefield.createSystems() or use prmtop and crd files,
         #  as in the openmmtools testsystems examples:
@@ -52,16 +51,22 @@ class WaterInWaterBox(TestSystem):
         # positions = inpcrd.getPositions(asNumpy=True)
         # self.topology, self.system, self.positions = topology, system, positions
 
+        # TODO: Other parameters? HydrogenMass, cutoffs, etc.?
+        self.num_atoms_per_solute = 3   # Water
+        self.num_atoms_per_solvent = 3  # Water
+        self.num_solvent_molecules = 8
+
         # pdb example:
         # Initial solute molecule
         pdb = app.PDBFile("/home/timsey/HDD/data/molecules/solvents/water.pdb")
-        # Add force field for molecule
-        forcefield = app.ForceField("amber14/tip3pfb.xml")
-        self.system = forcefield.createSystem(pdb.topology)
+        # NOTE: Add solute force field if not water!
         # Add solvent
         modeller = app.modeller.Modeller(pdb.topology, pdb.positions)
-        # TODO: Does this mutate self.system as well?
-        modeller.addSolvent(self.system, model='tip3p', numAdded=8)  # ‘tip3p’, ‘spce’, ‘tip4pew’, ‘tip5p’, ‘swm4ndp’
+        forcefield = app.ForceField("amber14/tip3p.xml")  # tip3pfb
+        # ‘tip3p’, ‘spce’, ‘tip4pew’, ‘tip5p’, ‘swm4ndp’
+        modeller.addSolvent(forcefield, model='tip3p', numAdded=self.num_solvent_molecules)
+        # Create system
+        self.system = forcefield.createSystem(modeller.topology)
 
         self.topology, self.positions = modeller.getTopology(), modeller.getPositions()
 
@@ -69,23 +74,13 @@ class WaterInWaterBox(TestSystem):
 class H2OinH2O(nn.Module, TargetDistribution):
     def __init__(
         self,
-        data_path=None,
         temperature=1000,
         energy_cut=1.0e8,
         energy_max=1.0e20,
         n_threads=4,
-        transform="z_matrix",
-        ind_circ_dih=[],
-        shift_dih=False,
-        shift_dih_params={"hist_bins": 100},
-        default_std={"bond": 0.005, "angle": 0.15, "dih": 0.2},
-        env="vacuum",
     ):
         """
         Boltzmann distribution of Alanine dipeptide
-        :param data_path: Path to the trajectory file used to initialize the
-            transformation, if None, a trajectory is generated
-        :type data_path: String
         :param temperature: Temperature of the system
         :type temperature: Integer
         :param energy_cut: Value after which the energy is logarithmically scaled
@@ -95,8 +90,6 @@ class H2OinH2O(nn.Module, TargetDistribution):
         :param n_threads: Number of threads used to evaluate the log
             probability for batches
         :type n_threads: Integer
-        :param transform: Which transform to use, can be mixed or internal
-        :type transform: String
         """
         super(H2OinH2O, self).__init__()
 
@@ -108,37 +101,17 @@ class H2OinH2O(nn.Module, TargetDistribution):
 
         system = WaterInWaterBox()
 
+        sim = app.Simulation(
+            system.topology,
+            system.system,
+            mm.LangevinIntegrator(temperature * unit.kelvin, 1.0 / unit.picosecond, 1.0 * unit.femtosecond),
+            mm.Platform.getPlatformByName("Reference"),
+        )
+
         # TODO: Do we want to run a quick simulation to get a sense of the coordinate magnitude for normalisation?
         #  If so, we can run a quick simulation here, as in aldp.py to obtain `transform_data`.
-        sim = None
 
-        # TODO: Transform should do:
-        #  Z --> X:
-        #   Add [0, 0, 0] coordinate of the solute.
-        #   Divide coordinates into blocks corresponding to water molecules.
-        #   Reorder based on the oxygen atom bond distances. This ensures molecular permutation invariance.
-        #    NOTE: This means we cannot use different base distributions for different types of bonds, because the
-        #     order with get mixed. Although we can do different things for the oxygen and hydrogen, since we're
-        #     fixing the block size. This works in general if we have a single type of solvent, and assume the solute
-        #     is the first block.
-        #   Maybe also reorder the hydrogen atoms based on the bond distance to their oxygen atom.
-        #   Starting from the first atom, revert coordinates to Cartesian.
-        #   Keep track of the det log Jacobian for all transformations: polar to Cartesian, scaling, rotations.
-        #    Q: What about reordering? Should we take the determinant of the permutation matrix? 1 or -1.
-        #  X --> Z:
-        #   Same, but now do the inverse coordinate transform after the blocking and reordering. The reordering works
-        #   both directions, because we can determine Cartesian distance easily from both Z and X coordinates.
-
-        self.coordinate_transform = WaterCoordinateTransform(
-            transform_data,
-            ndim,
-            z_matrix,
-            cart_indices,
-            mode=transform,
-            ind_circ_dih=ind_circ_dih,
-            shift_dih=shift_dih,
-            shift_dih_params=shift_dih_params,
-            default_std=default_std,)
+        self.coordinate_transform = WaterCoordinateTransform(system)
 
         if n_threads > 1:
             self.p = bg.distributions.TransformedBoltzmannParallel(
