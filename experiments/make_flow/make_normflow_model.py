system fixed, atom order checked, scaling presumably fixed, working on circular flow

experiments/make_flow/make_normflow_model.py
@@ -158,20 +158,47 @@ def make_wrapped_normflow_resampled_flow(
     return wrapped_dist
 
 
-def make_wrapped_normflow_solvent_flow(config, target):
+def make_wrapped_normflow_solvent_flow(config, target, periodic_inds):
     """
     Setup Flow model.
     """
 
     # Flow parameters
     flow_type = config["flow"]["type"]
+    seed = config["training"]["seed"]
     dim = target.dim
 
+    # TODO: Check that base distribution is correct. Check scales of base distribution at initialisation. I think it
+    #  might be initialised at [-pi, pi], instead of [0, pi]? Or even [-pi/2, pi/2], as a symmetric interval that has
+    #  width pi = bound_circ. Compare the Circular Flow implementation here:
+    #   https://github.com/francois-rozet/zuko/blob/master/zuko/flows.py#L493
+    #  And the used CircularShiftTransform: https://github.com/francois-rozet/zuko/blob/master/zuko/transforms.py
+    #  Note: MonotonicRQSTransform is simply the monotonic Rational Quadratic Spline transform.
+    #  What is our PeriodicShift doing exactly?
     # Base distribution
-    # TODO: We only use the Gauss here, because we have no freely rotating dihedrals that we would need circular
-    #  distributions for (circ_ind). Check that this is actually true!
+    # Indices of periodic variables (e.g., phi, theta) are given by `periodic_inds`, these should have their owns scale
+    # Original implementation uses 2 * pi / `std_of_angle` for base_scale of uniform distribution. I think it makes
+    #  more sense to not use the std_of_angle here, as the want the flow to operate on unit scale (also if
+    #  std_of_angle is not unit scale, then there will be a large difference between the scale of the Gaussian
+    #  base dist N(0, 1), and the scale of the uniform base dist U(0, 2 * pi / std_of_angle).
+    # Note that we have two different types of angles: phi and theta. But we can just use a pi range for both, and
+    #  multiply the one for phi by 2 at Flow output (in principle the Flow can learn that phi angles have double
+    #  range, but we might as well put that in manually, so that on initialisation the relative scale matches.
+    bound_circ = np.pi
+    # Bound of the Spline tails.
+    tail_bound = 5.0 * torch.ones(dim)
+    tail_bound[periodic_inds] = bound_circ
+
+    circ_shift = None if not "circ_shift" in config["flow"] else config["flow"]["circ_shift"]
+
+    # Base distribution
     if config["flow"]["base"]["type"] == "gauss":
         base = nf.distributions.DiagGaussian(dim, trainable=config["flow"]["base"]["learn_mean_var"])
+    elif config["flow"]["base"]["type"] == "gauss-uni":
+        base_scale = torch.ones(dim)  # Stddev of Gaussian or width of uniform
+        base_scale[periodic_inds] = bound_circ
+        base = nf.distributions.UniformGaussian(dim, periodic_inds, scale=base_scale)
+        base.shape = (dim,)
     else:
         raise NotImplementedError("The base distribution " + config["flow"]["base"]["type"] + " is not implemented")
 
@@ -181,15 +208,12 @@ def make_wrapped_normflow_solvent_flow(config, target):
     tail_bound = 5.0 * torch.ones(dim)
 
     for i in range(n_layers):
-        if flow_type == "ar-nsf":
+        if flow_type == "ar-nsf":  # Autoregressive Rational Spline Normalizing Flow
             bl = config["flow"]["blocks_per_layer"]
             hu = config["flow"]["hidden_units"]
             nb = config["flow"]["num_bins"]
             ii = config["flow"]["init_identity"]
             dropout = config["flow"]["dropout"]
-            # TODO: This is different from the Circular splines used in FAB paper. As far as I know, we
-            #  don't have freely rotating angles that we need the Circular indices for. But if we do, we should
-            #  change this.
             layers.append(
                 nf.flows.AutoregressiveRationalQuadraticSpline(
                     dim,
@@ -202,14 +226,11 @@ def make_wrapped_normflow_solvent_flow(config, target):
                     dropout_probability=dropout,
                 )
             )
-        elif flow_type == "coup-nsf":
+        elif flow_type == "coup-nsf":  # Coupling Rational Spline Normalizing Flow
             bl = config["flow"]["blocks_per_layer"]
             hu = config["flow"]["hidden_units"]
             nb = config["flow"]["num_bins"]
             dropout = config["flow"]["dropout"]
-            # TODO: This is different from the Circular splines used in FAB paper. As far as I know, we
-            #  don't have freely rotating angles that we need the Circular indices for. But if we do, we should
-            #  change this.
             layers.append(
                 nf.flows.CoupledRationalQuadraticSpline(
                     dim,
@@ -218,6 +239,48 @@ def make_wrapped_normflow_solvent_flow(config, target):
                     tail_bound=tail_bound,
                     num_bins=nb,
                     dropout_probability=dropout,
+                )
+            )
+        elif flow_type == "circ-ar-nsf":  # Circular AutoRegressive Rational Spline Normalizing Flow
+            bl = config["flow"]["blocks_per_layer"]
+            hu = config["flow"]["hidden_units"]
+            nb = config["flow"]["num_bins"]
+            ii = config["flow"]["init_identity"]
+            dropout = config["flow"]["dropout"]
+            layers.append(
+                nf.flows.CircularAutoregressiveRationalQuadraticSpline(
+                    dim,
+                    bl,
+                    hu,
+                    periodic_inds,
+                    tail_bound=tail_bound,
+                    num_bins=nb,
+                    permute_mask=True,
+                    init_identity=ii,
+                    dropout_probability=dropout,
+                )
+            )
+        elif flow_type == "circ-coup-nsf":  # Circular Coupled Rational Spline Normalizing Flow
+            bl = config["flow"]["blocks_per_layer"]
+            hu = config["flow"]["hidden_units"]
+            nb = config["flow"]["num_bins"]
+            ii = config["flow"]["init_identity"]
+            dropout = config["flow"]["dropout"]
+            if i % 2 == 0:
+                mask = nf.utils.masks.create_random_binary_mask(dim, seed=seed + i)
+            else:
+                mask = 1 - mask
+            layers.append(
+                nf.flows.CircularCoupledRationalQuadraticSpline(
+                    dim,
+                    bl,
+                    hu,
+                    periodic_inds,
+                    tail_bound=tail_bound,
+                    num_bins=nb,
+                    init_identity=ii,
+                    dropout_probability=dropout,
+                    mask=mask,
                 )
             )
         else:
@@ -230,6 +293,15 @@ def make_wrapped_normflow_solvent_flow(config, target):
 
         if config["flow"]["actnorm"]:
             layers.append(nf.flows.ActNorm(dim))
+
+        # Shift the periodic angles.
+        if i % 2 == 1 and i != n_layers - 1:
+            if circ_shift == "constant":
+                layers.append(nf.flows.PeriodicShift(periodic_inds, bound=bound_circ, shift=bound_circ))
+            elif circ_shift == "random":
+                gen = torch.Generator().manual_seed(seed + i)
+                shift_scale = torch.rand([], generator=gen) + 0.5
+                layers.append(nf.flows.PeriodicShift(periodic_inds, bound=bound_circ, shift=shift_scale * bound_circ))
 
         # SNF
         if "snf" in config["flow"]:

experiments/setup_run.py
@@ -165,10 +165,13 @@ def setup_model(cfg: DictConfig, target: TargetDistribution) -> FABModel:
     dim = cfg.target.dim  # applies to flow and target
     p_target = cfg.fab.loss_type not in ALPHA_DIV_TARGET_LOSSES or not cfg.training.prioritised_buffer
     if cfg.flow.solvent_flow:
-        # TODO: Check this is correct.
+        # Periodic indices for solvent are phi and theta. This is all indices except [0::3], which represent the
+        #  radial distance instead.
+        periodic_inds = np.array([i for i in range(dim) if i % 3 != 0])
         flow = make_wrapped_normflow_solvent_flow(
             cfg,
             target,
+            periodic_inds=periodic_inds,
         )
 
     elif cfg.flow.resampled_base:

fab/target_distributions/aldp.py
@@ -449,6 +449,11 @@ class InternalCoordinateTransform(Transform):
             self._setup_indices(z_indices, cart_indices)
             self._validate_data(data)
             # Setup the mean and standard deviations for each internal coordinate.
+            #  These are computed by transforming data X --> Z and then computing means and stds.
+            #  These values are then used to unnormalise the internal coordinates in the Z --> X direction,
+            #   so that the resulting output fits the values in the Boltzmann distribution. Note that the learned
+            #   values will still be off initially, as the current normalisation only makes it so the flow needs to
+            #   output closer-to-unit values (the flow still needs to learn to actually do this).
             transformed, _ = self._fwd(data)
             # Normalize
             self.default_std = default_std
@@ -838,7 +843,7 @@ class CompleteInternalCoordinateTransform(nn.Module):
         scale_jac = -(torch.log(self.std_b1) + torch.log(self.std_b2) + torch.log(self.std_angle))
         self.register_buffer("scale_jac", scale_jac)
 
-    def forward(self, x):
+    def forward(self, x):  # X --> Z
         # Create the jacobian vector
         jac = x.new_zeros(x.shape[0])
 
@@ -869,7 +874,7 @@ class CompleteInternalCoordinateTransform(nn.Module):
 
         return x, jac
 
-    def inverse(self, x):
+    def inverse(self, x):  # Z --> X
         # Create the jacobian vector
         jac = x.new_zeros(x.shape[0])
 

fab/target_distributions/h2o_in_h2o.py
@@ -1,8 +1,11 @@
 import torch
 from torch import nn
+import numpy as np
+import tempfile
+import pathlib
 
 from fab.target_distributions.base import TargetDistribution
-from fab.transforms.water_transform import WaterCoordinateTransform
+from fab.transforms.global_3point_spherical_transform import Global3PointSphericalTransform
 
 import boltzgen as bg
 from simtk import openmm as mm
@@ -46,7 +49,7 @@ class WaterInWaterBox(TestSystem):
         # TODO: Other parameters? HydrogenMass, cutoffs, etc.?
         self.num_atoms_per_solute = 3   # Water
         self.num_atoms_per_solvent = 3  # Water
-        self.num_solvent_molecules = (dim - self.num_atoms_per_solute) // self.num_atoms_per_solvent
+        self.num_solvent_molecules = (dim - self.num_atoms_per_solute) // (self.num_atoms_per_solvent * 3)
 
         # pdb example:
         # Initial solute molecule
@@ -61,28 +64,29 @@ class WaterInWaterBox(TestSystem):
         self.system = forcefield.createSystem(modeller.topology)
 
         self.topology, self.positions = modeller.getTopology(), modeller.getPositions()
+        # self.topology.atoms() yields the atom order, which is OHH OHH OHH etc.
+        # This is the order in which the coordinates are stored in the positions array.
+        self.atoms = [atom.name for atom in self.topology.atoms()]
 
 
 class H2OinH2O(nn.Module, TargetDistribution):
     def __init__(
         self,
-        dim=3 + 3 * 8,
-        temperature=1000,
-        energy_cut=1.0e8,
-        energy_max=1.0e20,
+        dim=3 * (3 + 3 * 8),  # 3 atoms in solute, 3 atoms in solvent, 8 solvent molecules. 3 dimensions per atom (xyz)
+        temperature=300,
+        energy_cut=1.0e8,  # TODO: Does this still make sense? Originally for 1000K ALDP.
+        energy_max=1.0e20,  # TODO: Does this still make sense? Originally for 1000K ALDP.
         n_threads=4,
+        data_path=None,
     ):
         """
         Boltzmann distribution of H2O in H2O.
         :param temperature: Temperature of the system
-            :type temperature: Integer
         :param energy_cut: Value after which the energy is logarithmically scaled
-            :type energy_cut: Float
         :param energy_max: Maximum energy allowed, higher energies are cut
-            :type energy_max: Float
         :param n_threads: Number of threads used to evaluate the log
             probability for batches
-            :type n_threads: Integer
+        :param data_path: Path to the data used for coordinate transform
         """
         super(H2OinH2O, self).__init__()
 
@@ -98,8 +102,33 @@ class H2OinH2O(nn.Module, TargetDistribution):
 
         # TODO: Do we want to run a quick simulation to get a sense of the coordinate magnitude for normalisation?
         #  If so, we can run a quick simulation here, as in aldp.py to obtain `transform_data`.
+        # Generate trajectory for coordinate transform if no data path is specified
+        if data_path is None:
+            traj_sim = app.Simulation(
+                system.topology,
+                system.system,
+                mm.LangevinIntegrator(temperature * unit.kelvin, 1.0 / unit.picosecond, 1.0 * unit.femtosecond),
+                platform=mm.Platform.getPlatformByName("Reference"),
+            )
+            traj_sim.context.setPositions(system.positions)
+            traj_sim.minimizeEnergy()
+            state = traj_sim.context.getState(getPositions=True)
+            position = state.getPositions(True).value_in_unit(unit.nanometer)
+            tmp_dir = pathlib.Path(tempfile.gettempdir())
+            data_path = tmp_dir / f"h2o_in_h2o_{dim}.pt"
 
-        self.coordinate_transform = WaterCoordinateTransform(system)
+            torch.save(torch.tensor(position.reshape(1, dim).astype(np.float64)), data_path)
+            del traj_sim
+
+        data_path = pathlib.Path(data_path)
+        assert data_path.suffix == ".pt", "Data path must be a .pt file"
+        transform_data = torch.load(data_path)
+        assert transform_data.shape[-1] == dim, (
+            f"Data shape ({transform_data.shape}) does not match number of "
+            f"coordinates in current system ({dim})."
+        )
+
+        self.coordinate_transform = Global3PointSphericalTransform(system, transform_data)
 
         if n_threads > 1:
             self.p = bg.distributions.TransformedBoltzmannParallel(