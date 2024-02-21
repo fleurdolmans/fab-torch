a22f92187c333219c099ad47cd637f363abeabd8
Timsey <tim_bb@hotmail.com>
2024-02-21 15:14:24 +0100

    prep for ivi running

--- run_scripts/h2o_in_h2o/0221_initial_hyperparams.sh
+++ run_scripts/h2o_in_h2o/0221_initial_hyperparams.sh
@@ -6,39 +6,41 @@ MAIN_DIR=/home/tbbakke/fab-torch
 LAUNCH_DIR=/home/tbbakke/fab-torch/launch/
 mkdir -p "${LAUNCH_DIR}"
 
-JOB_NAME=0221_hyperparams
 
-# Create dir for specific experiment run
-dt=$(date '+%F_%T.%3N')
-LOGS_DIR=${LAUNCH_DIR}/${dt}
-mkdir -p "${LOGS_DIR}"
-# Copy code to experiment folder
-rsync -arm ${MAIN_DIR} --stats --exclude-from=${MAIN_DIR}/"SYNC_EXCLUDE" ${LOGS_DIR};
-JOB_NAME=${JOB_NAME}
-SLURM=${LOGS_DIR}/run.slrm
-# Make SLURM file
-echo "${SLURM}"
-echo "#!/bin/bash" > ${SLURM}
-echo "#SBATCH --job-name=$JOB_NAME" >> ${SLURM}
-echo "#SBATCH --output=${LOGS_DIR}/%j.out" >> ${SLURM}
-echo "#SBATCH --error=${LOGS_DIR}/%j.err" >> ${SLURM}
-echo "#SBATCH --gres=gpu:1" >> ${SLURM}
-echo "#SBATCH --cpus-per-task=12" >> ${SLURM}
-echo "#SBATCH --ntasks-per-node=1" >> ${SLURM}
-echo "#SBATCH --mem=8G" >> ${SLURM}
-echo "#SBATCH --time=0-2:00:00" >> ${SLURM}
-echo "#SBATCH --nodes=1" >> ${SLURM}
-echo "export PYTHONPATH=:\$PYTHONPATH:" >> ${SLURM}
-{
-    echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/ml/bin/python ${LOGS_DIR}/alrl/run.py \
-        --data_type ${dataset} --wandb True --wandb_entity timsey --wandb_project al_imba \
-        --out_dir ${OUT_DIR} --abs_path_to_data_store /home/tbbakke/data/alrl/ \
-        --data_split_seed ${seed} --seed ${seed} --normalise_features True --stratify True \
-        --eval_split test --num_val_points 0 --num_reward_points 0 --num_annot_points ${num_annot} \
-        --imbalance_factors 1 10 1 10 1 10 1 10 1 10 --ignore_existing_imbalance False --class_weight_type balanced --use_class_weights_for_fit ${ucw} \
-        --classifier_type ${classifier} --acquisition_strategy ${method} --budget ${budget} --al_steps 10
-    echo
-} >> ${SLURM}
+BLOCKS=(12, 12, 12, 12, 16, 16, 16, 16)
+HIDDEN_UNITS=(256, 256, 512, 512, 256, 256, 512, 512)
+NUM_BINS=(9, 13, 9, 13, 9, 13, 9, 13)
 
-sbatch ${SLURM}
+
+for index in "${!BLOCKS[@]}"; do
+  JOB_NAME=0221_hyperparams
+
+  # Create dir for specific experiment run
+  dt=$(date '+%F_%T.%3N')
+  LOGS_DIR=${LAUNCH_DIR}/${dt}
+  mkdir -p "${LOGS_DIR}"
+  # Copy code to experiment folder
+  rsync -arm ${MAIN_DIR} --stats --exclude-from=${MAIN_DIR}/"SYNC_EXCLUDE" ${LOGS_DIR};
+  JOB_NAME=${JOB_NAME}
+  SLURM=${LOGS_DIR}/run.slrm
+  # Make SLURM file
+  echo "${SLURM}"
+  echo "#!/bin/bash" > ${SLURM}
+  echo "#SBATCH --job-name=$JOB_NAME" >> ${SLURM}
+  echo "#SBATCH --output=${LOGS_DIR}/%j.out" >> ${SLURM}
+  echo "#SBATCH --error=${LOGS_DIR}/%j.err" >> ${SLURM}
+  echo "#SBATCH --gres=gpu:1" >> ${SLURM}
+  echo "#SBATCH --cpus-per-task=12" >> ${SLURM}
+  echo "#SBATCH --ntasks-per-node=1" >> ${SLURM}
+  echo "#SBATCH --mem=8G" >> ${SLURM}
+  echo "#SBATCH --time=0-2:00:00" >> ${SLURM}
+  echo "#SBATCH --nodes=1" >> ${SLURM}
+  echo "export PYTHONPATH=:\$PYTHONPATH:" >> ${SLURM}
+  {
+      echo CUDA_VISIBLE_DEVICES=0 /home/tbbakke/anaconda3/envs/fab/bin/python ${LOGS_DIR}/fab-torch/experiments/solvation/run.py \
+          --flow.blocks ${BLOCKS[index]} --flow.hidden_units ${HIDDEN_UNITS[index]} --flow.num_bins ${NUM_BINS[index]}
+  } >> ${SLURM}
+
+  sbatch ${SLURM}
+  sleep .5
 
