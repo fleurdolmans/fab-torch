38c117cac133b3314ec06cf077d3d5ee19ca5514
Timsey <tim_bb@hotmail.com>
2024-04-16 18:21:26 +0200

    so2 big run

--- run_scripts/so2_in_h2o/0416_local_base.sh
+++ run_scripts/so2_in_h2o/0416_local_base.sh
@@ -4,5 +4,6 @@ source /home/timsey/anaconda3/bin/activate fab
 
 nvidia-smi
 
-PYTHONPATH=/home/timsey/Projects/fab-torch CUDA_VISIBLE_DEVICES=0 /home/timsey/anaconda3/envs/fab/bin/python \
+PYTHONPATH=/home/timsey/Projects/fab-torch LD_LIBRARY_PATH=/home/timsey/anaconda3/envs/fab/lib \
+CUDA_VISIBLE_DEVICES=0 /home/timsey/anaconda3/envs/fab/bin/python \
   /home/timsey/Projects/fab-torch/experiments/solvation/run.py --config-name so2inh2o_forwardkl.yaml
