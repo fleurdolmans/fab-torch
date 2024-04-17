fe070cfc40996c966d9b7f9011830664677d6724
Timsey <tim_bb@hotmail.com>
2024-04-17 13:41:02 +0200

    so2 big run overfitting

--- run_scripts/so2_in_h2o/0416_local_base.sh
+++ run_scripts/so2_in_h2o/0416_local_base.sh
@@ -4,6 +4,9 @@ source /home/timsey/anaconda3/bin/activate fab
 
 nvidia-smi
 
-PYTHONPATH=/home/timsey/Projects/fab-torch LD_LIBRARY_PATH=/home/timsey/anaconda3/envs/fab/lib \
-CUDA_VISIBLE_DEVICES=0 /home/timsey/anaconda3/envs/fab/bin/python \
-  /home/timsey/Projects/fab-torch/experiments/solvation/run.py --config-name so2inh2o_forwardkl.yaml
+HOME_DIR=/home/timsey
+CONDA_ENV_DIR=${HOME_DIR}/anaconda3/envs/fab
+PROJECT_DIR=${HOME_DIR}/Projects/fab-torch
+
+CUDA_VISIBLE_DEVICES=0 PYTHONPATH=${PROJECT_DIR} LD_LIBRARY_PATH=${CONDA_ENV_DIR}/lib \
+  ${CONDA_ENV_DIR}/bin/python ${PROJECT_DIR}/experiments/solvation/run.py --config-name so2inh2o_forwardkl.yaml
