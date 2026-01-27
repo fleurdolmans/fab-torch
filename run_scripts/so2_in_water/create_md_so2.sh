aa0d1c0e43162e3454025d238dd1662150c4f469
Fleur Dolmans <fdolmans@int6.local.snellius.surf.nl>
2026-01-27 14:18:22 +0100

    Experiment with MD simulations

--- run_scripts/so2_in_water/create_md_so2.sh
+++ run_scripts/so2_in_water/create_md_so2.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=9
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_a100
-#SBATCH --time=01:40:00
+#SBATCH --time=02:00:00
 
 module purge
 module load 2025
@@ -42,4 +42,8 @@ export MAIN_DIR="${MAIN_DIR}"
 # Create MD data for SO2 in water and plot diagnostics
 srun python "${MAIN_DIR}/experiments/solvation/create_md_data.py" \
   --config-name make_md_data \
-  solute_name=so2 
+  solute_name=so2 \
+  simulation_version="v4" \
+  num_solvent_molecules=95 \
+  constraint_radius=0.8 \
+  report_interval=5000 
