56ecaaadd436fe462b8e994e28d02681e5bd47cc
Fleur Dolmans <fdolmans@int4.local.snellius.surf.nl>
2026-06-01 14:00:16 +0200

    test droplet

--- run_scripts/molecule/so2_in_water/droplet/SO2InWater_droplet_forwardkl.sh
+++ run_scripts/molecule/so2_in_water/droplet/SO2InWater_droplet_forwardkl.sh
@@ -48,7 +48,7 @@ cat > "${SLURM}" <<EOF
 #SBATCH --cpus-per-task=9
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_a100
-#SBATCH --time=00:05:00
+#SBATCH --time=01:00:00
 
 module purge
 module load 2025
