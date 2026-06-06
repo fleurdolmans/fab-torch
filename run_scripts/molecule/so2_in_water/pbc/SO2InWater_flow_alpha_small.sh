a8a5792eaed115243f9afd03175693da3c9363f7
Fleur Dolmans <fdolmans@int4.local.snellius.surf.nl>
2026-06-06 13:02:13 +0200

    changes

--- run_scripts/molecule/so2_in_water/pbc/SO2InWater_flow_alpha_small.sh
+++ run_scripts/molecule/so2_in_water/pbc/SO2InWater_flow_alpha_small.sh
@@ -76,16 +76,16 @@ python ${LOGS_DIR}/${PROJECT_NAME}/experiments/solvation/run.py \\
   target.box_length_nm=0.9 target.nonbonded_cutoff_nm=0.4 target.num_solvent_molecules=22 \\
   target.internal_constraints=hbonds target.rigid_water=true \\
   target.energy_cut=1e4 target.energy_max=1e10 \\
-  target.boundary_condition=pbc fab.loss_type=flow_reverse_kl fab.use_ais=false \\
+  target.boundary_condition=pbc fab.loss_type=flow_alpha_2_div_nis fab.use_ais=false \\
   flow.hidden_units=128 flow.base.type=gauss flow.base.learn_mean_var=false flow.type=perm-equi-torus-nf \\
   flow.layers=12 flow.blocks_per_layer=4 flow.group_size=6 flow.tail_bound=3 \\
-  training.checkpoint_load_dir=/home/fdolmans/HDD/results/fab/SoluteInwater/so2_in_water/MD_training/2026-06-05/19-33-11_399924 \\
+  training.checkpoint_load_dir=/home/fdolmans/HDD/results/fab/SoluteInwater/so2_in_water/MD_training/2026-06-05/13-19-21_272406 \\
   training.lr=1e-4 training.wd=1e-6 training.batch_size=256 evaluation.eval_batch_size=64 \\
-  training.max_grad_norm=1 training.warmup_iter=250\\
+  training.max_grad_norm=1 training.warmup_iter=500\\
   target.transform.canonical_sorting=true target.transform.version=LGT\\
-  training.overlap_penalty=100 training.mixing=0.0 training.energy_mode=full\\
-  training.n_iterations=1000 training.buffer.use=false training.buffer.prioritised=false training.lr_scheduler.decay_iter=1000 \\
-  evaluation.n_eval=5 evaluation.n_plots=5 evaluation.n_checkpoints=1
+  training.overlap_penalty=10 training.mixing=0.0 training.energy_mode=full\\
+  training.n_iterations=2000 training.buffer.use=false training.buffer.prioritised=false training.lr_scheduler.decay_iter=2000 \\
+  evaluation.n_eval=10 evaluation.n_plots=10 evaluation.n_checkpoints=1
 EOF
 
 
