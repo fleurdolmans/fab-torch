fc510570c23ec59cbf0cce34cfffe7622a4a6a2e
Fleur Dolmans <fdolmans@int4.local.snellius.surf.nl>
2026-06-07 14:17:43 +0200

    changes

--- run_scripts/molecule/so2_in_water/pbc/SO2InWater_fab_pbuff_small.sh
+++ run_scripts/molecule/so2_in_water/pbc/SO2InWater_fab_pbuff_small.sh
@@ -70,16 +70,18 @@ python ${LOGS_DIR}/${PROJECT_NAME}/experiments/solvation/run.py \\
   --config-name SoluteInSolvent \\
   target.solute_name=${SOLUTE} target.solvent_name=${SOLVENT} target.simulation_version=v10 \\
   target.box_length_nm=0.9 target.nonbonded_cutoff_nm=0.4 target.num_solvent_molecules=22 \\
-  target.internal_constraints=hbonds target.rigid_water=true target.transform_version=v1\\
+  target.internal_constraints=hbonds target.rigid_water=true \\
   target.energy_cut=1e6 target.energy_max=1e9 \\
   target.boundary_condition=pbc fab.loss_type=fab_alpha_div fab.use_ais=true \\
   flow.hidden_units=128 flow.layers=12 flow.blocks_per_layer=4 flow.group_size=6 flow.tail_bound=3\\
-  flow.base.type=structured-gauss flow.base.learn_mean_var=true flow.type=coupled-spline-nf\\
-  fab.n_intermediate_distributions=32 fab.transition_operator.n_inner_steps=6 fab.transition_operator.init_step_size=0.01 \\
+  flow.base.type=gauss flow.base.learn_mean_var=false flow.type=perm-equi-torus-nf\\
+  fab.n_intermediate_distributions=8 fab.transition_operator.n_inner_steps=6 fab.transition_operator.init_step_size=0.05 \\
+  training.checkpoint_load_dir=/home/fdolmans/HDD/results/fab/SoluteInwater/so2_in_water/MD_training/2026-06-05/19-33-11_399924 \\
   training.lr=5e-5 training.wd=1e-6 training.batch_size=128 evaluation.eval_batch_size=128\\
   training.buffer.maximum_length=32768 training.buffer.min_length=4096 \\
+  target.transform.canonical_sorting=true target.transform.version=LGT training.overlap_penalty=1.0\\
   training.max_grad_norm=10 training.buffer.n_batches_sampling=4 training.buffer.w_adjust_max_clip=1\\
-  training.warmup_iter=25 training.n_iterations=50 training.lr_scheduler.decay_iter=50\\
+  training.warmup_iter=25 training.n_iterations=100 training.lr_scheduler.decay_iter=100\\
   evaluation.n_eval=10 evaluation.n_plots=10 evaluation.n_checkpoints=1 
 EOF
 
